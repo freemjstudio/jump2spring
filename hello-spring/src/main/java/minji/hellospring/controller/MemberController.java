303878999d113ceae70de359057f620beee0a46c
freemjstudio <mjwoo0@naver.com>
2024-03-25 13:43:14 +0900

    add:MemberForm Business Logic

--- hello-spring/src/main/java/minji/hellospring/controller/MemberController.java
+++ hello-spring/src/main/java/minji/hellospring/controller/MemberController.java
@@ -5,6 +5,7 @@ import minji.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 // spring 이 MemberController 객체를 생성해서 넣어둠
 @Controller
@@ -21,5 +22,14 @@ public class MemberController {
     @GetMapping("/members/new")
     public String createForm() {
         return "members/createMemberForm";
+    }
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
