05bf8371b06b03b37cb19e0233b072e4e7ed5419
freemjstudio <mjwoo0@naver.com>
2024-03-27 18:02:03 +0900

    add:traditional JDBC

--- hello-spring/src/main/java/minji/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/minji/hellospring/SpringConfig.java
@@ -1,14 +1,26 @@
 package minji.hellospring;
 
 import minji.hellospring.domain.Member;
+import minji.hellospring.repository.JdbcMemberRepository;
 import minji.hellospring.repository.MemberRepository;
 import minji.hellospring.repository.MemoryMemberRepository;
 import minji.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource; // spring bean 이 관리한다.
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -16,6 +28,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+        //return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
